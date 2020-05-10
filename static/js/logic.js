c2f4a0c257ea790d2fdeba58e144d025ed38107f
Paul G. Arias <paul.g.arias@gmail.com>
2020-05-09 23:22:42 -0400

    Updated logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,7 +1,8 @@
 // Creating map object
 var map = L.map("map", {
-  center: [39.9258333, -74.75],
-  zoom: 9
+  center: [38., -96.],
+  //center: [0,0],
+  zoom: 4
 });
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
@@ -25,11 +26,27 @@ d3.json("static/resource/all_us_diocese.json", function(data) {
   var coords;
   console.log(data.features.dioceses)
   for (var i=0; i<data.features.dioceses.length; i++) {
-  		L.polygon(data.features.dioceses[i].geometry.coordinates, {
-  			color: "grey",
-  			fillColor: "yellow",
-  			fillOpacity: 0.5,
+  		if(data.features.dioceses[i].geometry.coordinates[0].length>1){
+			console.log(data.features.dioceses[i].name)
+			console.log(data.features.dioceses[i].geometry.coordinates[0].length)
+		}
+  		var polygons = L.polygon(data.features.dioceses[i].geometry.coordinates, {
+  			color: '#0000FF',
+			weight: 2,
+			opacity: 0.2,
+  			fillColor: '#0000FF',
+  			fillOpacity: 0.2,
   			weight: 1
-		}).addTo(map);
+		}) //.addTo(map);
+		polygons.addTo(map);
+		//polygons.setStyle({fillColor: '#0000FF',fillOpacity:"0.5"});
+		polygons.on('mouseover', function(ev) {
+                	//console.log(ev.target); // ev is an event object (MouseEvent in this case)
+                        ev.target.setStyle({fillOpacity:0.5});
+			//console.log(ev.target.getBounds()); //useful to zoom into
+
+        	}).on('mouseout', function(ev) {
+			ev.target.setStyle({fillOpacity:0.2});
+		});
    }
 });
