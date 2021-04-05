91ae289e9b191c83fb540a20f77a9405ebb933a3
Paul G. Arias <paul.g.arias@gmail.com>
2021-04-05 16:57:09 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,10 +5,10 @@ var map = L.map("map", {
   zoom: 4
 });
 
-L.tileLayer("https://api.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+L.tileLayer("https://api.mapbox.com/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
-  id: "mapbox.streets",
+  id: "mapbox/streets-11",
   accessToken: API_KEY
 }).addTo(map);
 
